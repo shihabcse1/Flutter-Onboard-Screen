5e1d734553206954b0b458b4c4f36dbfea2736ea
shihabcse1 <tomdshihab@gmail.com>
2022-08-04 02:49:02 +0600

    overall colors added

--- lib/OnBoarding.dart
+++ lib/OnBoarding.dart
@@ -11,32 +11,108 @@ class OnBoarding extends StatefulWidget {
 }
 
 class _OnBoardingState extends State<OnBoarding> {
+  int currentIndex = 0;
+  PageController? _controller;
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        itemCount: contents.length,
-          itemBuilder: (_ , i){
-            return Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(
-                children: [
-                  SvgPicture.asset(
-                    contents[i].image!,
-                    height: 300,
-                  ),
-                  Text(
-                    contents[i].title!,
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          }
+      appBar: AppBar(
+        leading: Text("Hello"),
+        backgroundColor: Color(0xffE0115F),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _controller,
+              itemCount: contents.length,
+                onPageChanged: (int index){
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+                itemBuilder: (_ , i){
+                  return Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          contents[i].title!,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffE0115F),
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        SvgPicture.asset(
+                          contents[i].image!,
+                          height: 300,
+                        ),
+                      ],
+                    ),
+                  );
+                }
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  contents.length,
+                      (index) => buildDot(index, context),
+              ),
+            ),
+          ),
+          Container(
+            height: 55,
+            margin: EdgeInsets.all(40.0),
+            width: double.infinity,
+            color: Colors.white,
+            child: FlatButton(
+              child: Text("Get Started"),
+              onPressed: (){
+                if(currentIndex == contents.length - 1){
+                  _controller?.nextPage(
+                      duration: Duration(milliseconds: 100),
+                      curve: Curves.bounceIn,
+                  );
+                }
+              },
+              color: Color(0xffE0115F),
+              textColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+       height: 10,
+       width: 10,
+       margin: EdgeInsets.only(right: 5),
+       decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Color(0xffE0115F),
+       ),
     );
   }
 }
