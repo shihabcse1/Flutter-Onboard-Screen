e183d8a42de3335529f99ec92c58a2b228c6574d
shihabcse1 <tomdshihab@gmail.com>
2022-08-18 21:48:36 +0600

    Firebase Signin with Google Initial setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding_screen/Pages/OnBoardingPage/OnBoarding.dart';
 
-void main() {
+Future main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
